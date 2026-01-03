3f32092f416d25edfc6a4033c986866e4b87b839
preettt10 <preet.trpi2005@gmail.com>
2026-01-03 13:53:31 +0530

    updated app.jsx for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import Login from './pages/login';
-import Register from './pages/Register'; // New Import
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import EmployeeProfile from './pages/EmployeeProfile';
 // Admin Imports
 import EmployeeList from './pages/admin/EmployeeList';
 import LeaveRequests from './pages/admin/LeaveRequests';
 import PayrollList from './pages/admin/PayrollList';
+import AdminAttendance from './pages/admin/AdminAttendance'; // NEW IMPORT
 // Employee Imports
 import Attendance from './pages/employee/Attendance';
 import LeaveApplication from './pages/employee/LeaveApplication';
@@ -21,7 +22,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout><LandingPage /></Layout>} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} /> {/* New Route */}
+      <Route path="/register" element={<Register />} />
       
       <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
       <Route path="/profile" element={<Layout><EmployeeProfile /></Layout>} />
@@ -30,6 +31,7 @@ function App() {
       <Route path="/admin/employees" element={<Layout><EmployeeList /></Layout>} />
       <Route path="/admin/leaves" element={<Layout><LeaveRequests /></Layout>} />
       <Route path="/admin/payroll" element={<Layout><PayrollList /></Layout>} />
+      <Route path="/admin/attendance" element={<Layout><AdminAttendance /></Layout>} /> {/* NEW ROUTE */}
 
       {/* Employee Routes */}
       <Route path="/employee/attendance" element={<Layout><Attendance /></Layout>} />
